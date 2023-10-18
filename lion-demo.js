cb360ed32d7933e960b55925df2e7be2bd1470f8
Konstantinos Norgias <Konstantinos.Norgias@ing.com>
2023-10-18 14:39:14 +0200

    the lion state

--- lion-demo.js
+++ lion-demo.js
@@ -1,15 +1,10 @@
-import { html, LitElement, ScopedElementsMixin } from 'ing-web/lit-2.js';
-import { IngButton } from 'ing-web/button.js';
-import { IngTooltip } from 'ing-web/tooltip.js';
+import { html, LitElement } from 'lit';
 
-export class LionDemo extends ScopedElementsMixin(LitElement) {
-    static get scopedElements() {
-    return {
-      'ing-button': IngButton,
-      'ing-tooltip': IngTooltip
-    };
-    }
+// As a side-effect this way of importing defines the custom elements, eg. <lion-button>, ready for use
+import '@lion/ui/define/lion-button.js';
+import '@lion/ui/define/lion-tooltip.js';
 
+export class LionDemo extends LitElement {
   static properties = {
     header: { type: String },
     counter: { type: Number },
@@ -22,10 +17,10 @@ export class LionDemo extends ScopedElementsMixin(LitElement) {
   render() {
     return html`
       <h1>${this.header}! Increment is at Nr: ${this.counter}</h1>
-      <ing-tooltip has-arrow>
-        <ing-button slot="invoker" @click=${() => {this.counter += 1; console.log(this.counter);}}>increment</ing-button>
+      <lion-tooltip has-arrow>
+        <lion-button slot="invoker" @click=${() => {this.counter += 1; console.log(this.counter);}}>increment</lion-button>
         <span slot="content"> +1 </span>
-      </ing-tooltip>
+      </lion-tooltip>
     `;
   }
 }
